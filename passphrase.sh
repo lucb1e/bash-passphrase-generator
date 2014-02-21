00849428bfb5a25b1496cb8da440206ed4740061
Luc <lucb1e@users.noreply.github.com>
2014-02-21 01:23:22 +0100

    Removed some left-over, added a few comments

--- passphrase.sh
+++ passphrase.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+# Run a couple searches at once since it's pretty badly written, err, cpu intensive.
 cpus=`cat /sys/devices/system/cpu/possible | tr "-" " " | awk '{print $2}'`;
 while :; do
 	let i=0;
-	while [ $i -lt $(($cpus)) ]; do
+	while [ $i -lt $cpus ]; do
 		bash randomword.sh &
 		let i=$i+1;
 	done;
 	sleep 0.1;
-	bash randomword.sh;
+	bash randomword.sh; # This way it finishes right about when the others do as well
 done;
